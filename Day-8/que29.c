4cbcf56dd81e2e8e9f3408c302cad0961a8c6752
Harsh Vishwakarma <harsh.vishwakarma2025@glbajajgroup.org>
2026-06-16 08:26:18 +0530

    Day-8 assignment is uploaded

---
+++ Day-8/que29.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    //Q29: Write a program to Print half pyramid pattern.
+    int i,j;
+    for(i=1;i<=5;i++){
+        for(j=1;j<=i;j++){
+        printf("* ");
+        }
+    printf("\n");
+}
+    return 0;
+}
